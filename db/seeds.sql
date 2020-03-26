97aa36a60455ba1929500466f474308211c5420c
Swedish <tarthurf@yahoo.com>
2020-03-26 12:11:11 -0700

    Created schema and seeds file, set connection.js to db, set server.js

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO TABLE burgers (name) VALUES ('Classic Cheeseburger')
+INSERT INTO TABLE burgers (name) VALUES ('Bacon Ranch Cheese Burger')
+INSERT INTO TABLE burgers (name) VALUES ('Nashville Hot Chicken Burger')
